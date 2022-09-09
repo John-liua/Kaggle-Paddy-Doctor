95b583efd43372a954cf7d3ee0a3034f996753a8
John Liu <89292585+John-liua@users.noreply.github.com>
2022-09-09 20:55:51 +0800

    Add files via upload

---
+++ Paddy_Doctor/block/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*
+
+# -------------------------------------------------------------------------------
+# Author: LiuNing
+# Contact: 2742229056@qq.com
+# Software: PyCharm
+# File: __init__.py
+# Time: 8/21/19 7:54 PM
+# Description: init
+# -------------------------------------------------------------------------------
+
+from .adaptive_avgmax_pool import *
+from .densenet import *
+from .helpers import *
+from .inception import *
+from .registry import *
+from .resnet import *
+from .senet import *
